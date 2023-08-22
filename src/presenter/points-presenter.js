d02d13e7a6c3144ebbabe3e09334b073000a7ac0
Maria-Bogatyreva <63573326+Maria-Bogatyreva@users.noreply.github.com>
2023-08-22 18:56:07 +0700

    Выделила функцию отрисовки точки в отдельный приватный метод

--- src/presenter/points-presenter.js
+++ src/presenter/points-presenter.js
@@ -21,17 +21,27 @@ export default class PointsPresenter {
     this.#pointsModel = pointsModel;
   }
 
+  // Отдельный приватный метод для отрисовки ОДНОЙ ТОЧКИ
+  #renderPoint({point, destinations, offers}) {
+    const pointComponent = new PointView({point, destinations, offers});
+    render(pointComponent, this.#pointListComponent.element);
+  }
+
   init() {
     this.#points = [...this.#pointsModel.points]; // Это наш массив точек, которые мы отрисовываем
     this.#destinations = [...this.#pointsModel.destinations];
     this.#offers = [...this.#pointsModel.offers];
 
     render(this.#pointListComponent, this.#presenterContainerElement); // вставили обертку ul
+
     // Вставляем ФОРМУ СОЗДАНИЯ/РЕДАКТИРОВАНИЯ ТОЧКИ. Передаем объект с точкой, а также массивы destinations и offers
     render(new EditPointFormView({point: this.#points[0], destinations: this.#destinations, offers: this.#offers}), this.#pointListComponent.element);
 
-    for (let i = 1; i < this.#points.length; i++) {
-      render(new PointView({point: this.#points[i], destinations: this.#destinations, offers: this.#offers}), this.#pointListComponent.element);
+    // Вставляем ТОЧКИ, пользуясь приватным методом
+    for (let i = 0; i < this.#points.length; i++) {
+     this.#renderPoint({point: this.#points[i], destinations: this.#destinations, offers: this.#offers});
     }
   }
+
+
 }
