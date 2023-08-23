a17ad29ef62cf30fd5a4951c944ba35ed55fd85d
Maria-Bogatyreva <63573326+Maria-Bogatyreva@users.noreply.github.com>
2023-08-23 14:53:50 +0700

    Доработки по замечаниям ревью

--- src/view/point-view.js
+++ src/view/point-view.js
@@ -39,7 +39,7 @@ function createPointTemplate(point, destinationsList, OffersList) {
                 <div class="event__type">
                   <img class="event__type-icon" width="42" height="42" src="img/icons/${type}.png" alt="Event type icon">
                 </div>
-                <h3 class="event__title">${type} ${pointDestinationObj ? pointDestinationObj.name : ''}</h3>
+                <h3 class="event__title">${type} ${pointDestinationObj?.name || ''}</h3>
                 <div class="event__schedule">
                   <p class="event__time">
                     <time class="event__start-time" datetime="${dateFullStart}T${timeStart}">${timeStart}</time>
